6205ac6dcc5c4302787adf9b9be6c7b5b9582e50
calm428 <ollie21428@gmail.com>
2023-11-04 00:02:07 +0100

    fix: font issue

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en" className="scroll-smooth">
-            <Head />
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;700&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
