746fe4250fb318f07950cfdfd9dbf1f9919006da
calm428 <ollie21428@gmail.com>
2023-11-08 14:37:01 +0800

    feat: add function to count number of visitors

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
-import { Layout } from "@components/Layout";
 import { HomeBanner } from "@components/Banner";
-import { IntroSection } from "@components/Intro";
-import { HowItWorksSection } from "@components/HowItWorks";
-import { FeatureSection } from "@components/Feature";
-import { SimAndVerifySection } from "@components/SimAndVerify";
-import { LicenseSection } from "@components/License";
 import { ContactSection } from "@components/Contact";
-import { TestimonialSection } from "@components/Testimonial";
+import { FeatureSection } from "@components/Feature";
+import { HowItWorksSection } from "@components/HowItWorks";
+import { IntroSection } from "@components/Intro";
+import { Layout } from "@components/Layout";
+import { LicenseSection } from "@components/License";
 import SEO from "@components/SEO/SEO";
+import { SimAndVerifySection } from "@components/SimAndVerify";
+import { TestimonialSection } from "@components/Testimonial";
+import { useEffect } from "react";
 
 export default function Home() {
+
+    useEffect(() => {
+        const sendData = async () => {
+            try {
+                const response = await fetch(
+                    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/api/visitor`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({}), // Replace with the request body if needed
+                    }
+                );
+                const data = await response.json();
+
+                console.log(data, "I am here", divRef.current)
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        if (typeof window !== "undefined") {
+            // This will only run in the browser where window is defined
+            sendData();
+        }
+    }, []);
+
     return (
         <Layout className="">
             <SEO
